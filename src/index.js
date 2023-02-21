7b7763c3b3d481efedce98787861f567835df8b1
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-02-21 15:31:02 +0100

    Setup to-do list structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+let MyTasks = [
+  { todoEntry: 'wash plates', index: 0, completed: false},
+  { todoEntry: 'wash cars', index: 1, completed: false},
+  { todoEntry: 'clean office', index: 2, completed: false},
+];
 
-    return element;
+// functions
+
+function addTasks() {
+  const todoList = document.querySelector('.list');
+  for (let i = 0; i < MyTasks.length; i++) {
+    const todo = MyTasks[i];
+    const listItem = document.createElement('li');
+    listItem.classList.add('listItem');
+    listItem.innerHTML = `
+      <input type="checkbox" name="" id="">
+      <p>${todo.todoEntry}</p>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    `;
+    todoList.appendChild(listItem);
   }
- 
-  document.body.appendChild(component());
- 
+}
+
+// Event listeners
+window.addEventListener('DOMContentLoaded', addTasks)
