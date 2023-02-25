dbc6a7f80e1ee54817e7e7ba0b1e032fb9bee811
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-02-25 12:03:46 +0100

    Add interaction

--- dist/runtime.js
+++ dist/runtime.js
@@ -163,6 +163,11 @@
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nc = undefined;
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	
