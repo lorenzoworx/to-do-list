e4a8654357d4864ec7ab43b372311aac14b82266
ikoote1 <ikootepreim@gmail.com>
2023-03-02 18:12:13 +0300

    Added remove code

--- src/modules/__tests__/add-remove.test.js
+++ src/modules/__tests__/add-remove.test.js
@@ -14,5 +14,18 @@ describe('addTask', () => {
     expect(list).toHaveLength(1);
   });
 
+  test('Remove one item from the list',()=>{
+    document.body.innerHTML = '<div>'
+    + '  <ul class="list"></ul>'
+    + '</div>';
+    
+  const myList = new List();
+  myList.addTask('washes');
+  myList.addTask('my wishes');
+  myList.deleteTask(0);
+    const list = document.querySelectorAll('.list li');
+    expect(list).toHaveLength(1);
+  })
+
 });
 
