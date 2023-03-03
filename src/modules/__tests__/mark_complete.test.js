774976fff92596be3f9c69417d4aff336794bbde
ikoote1 <ikootepreim@gmail.com>
2023-03-03 13:46:33 +0300

    Added the clear, mark, and edit .test.js files for testing

---
+++ src/modules/__tests__/mark_complete.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+import List from "../crud";
+
+describe('Mark Completed', () => {
+  test('Completed task set to true', () => {
+    document.body.innerHTML = '<div>'
+    + '  <ul class="list"></ul>'
+    + '</div>';
+    const testObj = {
+      index: 0,
+      description: 'test',
+      completed: false,
+    };
+    const myList = new List();
+    // Add test tas
+    myList.addTask(testObj);
+
+    // Mark as Complete
+    const completed = true;
+    myList.markComplete(testObj.index, completed);
+    myList.displayTasks();// Update html.
+    const checkbox = document.getElementById('checkbox');
+
+    expect(checkbox.checked).toBe(completed);
+  });
+});
