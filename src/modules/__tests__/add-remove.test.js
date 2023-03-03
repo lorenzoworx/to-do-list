7bcd2170b941069b8c6e81e36b2f57e1de8604c4
ikoote1 <ikootepreim@gmail.com>
2023-03-03 10:49:20 +0300

    Fixed all test errors

--- src/modules/__tests__/add-remove.test.js
+++ src/modules/__tests__/add-remove.test.js
@@ -13,4 +13,16 @@ describe('addTask', () => {
     const list = document.querySelectorAll('.list li');
     expect(list).toHaveLength(1);
   });
+
+  test('Remove one item from the list', () => {
+    document.body.innerHTML = '<div>'
+      + '  <ul class="list"></ul>'
+      + '</div>';
+    const myList = new List();
+    myList.addTask('wash dishes');
+    myList.addTask('clean the house');
+    myList.deleteTask(1);
+    const list = document.querySelectorAll('.list li');
+    expect(list).toHaveLength(1);
+  });
 });
