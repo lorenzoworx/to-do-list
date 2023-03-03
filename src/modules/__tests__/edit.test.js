4fc44e4a09833904669da735172afb69f3f464ea
ikoote1 <ikootepreim@gmail.com>
2023-03-03 14:00:07 +0300

    Fixed some errors

--- src/modules/__tests__/edit.test.js
+++ src/modules/__tests__/edit.test.js
@@ -1,23 +1,3 @@
 /**
  * @jest-environment jsdom
  */
-import List from "../crud";
-
-describe('Edit Items in list', () => {
-  test('Edited task textContent should be equal to updated description', () => {
-    document.body.innerHTML = '<div>'
-    + '  <ul class="list"></ul>'
-    + '</div>';
-    const testdescription = 'test';
-    const myList = new List();
-    // Add test task
-    myList.addTask(testdescription);
-    const index = document.querySelector('.listItem').id;
-    // Update test task
-    const newDescription = 'Test is updated';
-    myList.updateTask(index, newDescription);
-    myList.displayList();
-    const description = document.getElementById('label');
-    expect(description.textContent).toBe(newDescription);
-  });
-});
